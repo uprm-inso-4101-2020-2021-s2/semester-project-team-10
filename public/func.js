863ec9aeb789060751dba14363b77db84b4b5536
Janilet <60708645+Janilet@users.noreply.github.com>
2021-02-25 00:14:39 -0400

    Added Count Section
    Fix social-icon issue with footer

--- public/func.js
+++ public/func.js
@@ -1,2 +1,10 @@
-
-
+$(document).ready(function() {
+    $('.counter').each(function () {
+        $(this).prop('Counter',0).animate(
+            { Counter: $(this).text()}, { 
+                duration: 4000,easing: 'swing',step: function (now) {
+                    $(this).text(Math.ceil(now));
+                }
+        });
+    });
+});
