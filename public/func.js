777ec8a08916600fd64e8af9615d48741e1c681a
Walbert Garcia Cruz <46732251+SoftWal@users.noreply.github.com>
2021-03-11 22:41:30 -0400

    Enhance sign in

--- public/func.js
+++ public/func.js
@@ -8,3 +8,22 @@ $(document).ready(function() {
         });
     });
 });
+
+// Sign in 
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Sign in 
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
